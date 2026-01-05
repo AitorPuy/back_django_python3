9639d5e64313ba875f83c81b4b69d92d2dae1b5e
aitorpuyprog <aitorpuyprog@gmail.com>
2026-01-05 20:24:48 +0100

    Refactorización de dependencias en requirements para optimizar compatibilidad y rendimiento

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if origin.strip()]
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
 
 #CSRF_TRUSTED_ORIGINS = [
